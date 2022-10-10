e394fb9c74999f823c75b4a1617d9ef4788a54fc
muratA <muratriyum@gmail.com>
2022-10-10 23:30:57 +0300

    Cydeo

---
+++ src/CarInsurance.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class CarInsurance {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Cydeo car insurance!");
+        Scanner scan=new Scanner(System.in);
+        double prim=0;
+        System.out.println("Enter your full Name");
+        String fullName=scan.nextLine();
+        System.out.println("Do you have a US diriver Licence?");
+        if(scan.nextLine()=="no") {
+            System.out.println("You must have a license to get insurance!");
+            System.exit(0);
+        }
+        System.out.println("Enter your zip code");
+        long zipCode=scan.nextLong();
+        if (zipCode==20910 || zipCode==20740){
+            prim+=60;
+        }else if (zipCode==22102 || zipCode==22103){
+            prim+=30;
+        }else {
+            prim+=50;
+        }
+        System.out.println("Is this vehicle owned, financed, or leased?");
+        String have=scan.nextLine();
+        switch (have){
+            case "owned":{
+                prim+=10;
+                break;
+            }
+            case "financed":{
+                prim+=15;
+                break;
+            }
+            case "leased":{
+                prim+=20;
+                break;
+            }
+        }
+
+        System.out.println("How is this vehicle primarily used?");
+        String have1=scan.nextLine();
+        if (have1 == "busines") {
+            prim+=50;
+        } else if (have1=="pleasure") {
+            prim+=10;
+        } else if (have1=="commuting") {
+            prim+=20;
+            System.out.println("`How many days do you commute?");
+            int dayCommute=scan.nextInt();
+            prim+=dayCommute*5;
+        }
+        System.out.println("How old are you?");
+        int old=scan.nextInt();
+        if (old<16){
+            System.out.println("You can't be driving");
+            System.exit(0);
+        }else if(old>=16 && old<20){
+            prim=prim*10;
+        } else if (old>=20 && old<25) {
+            prim=prim*6;
+        } else if (old>=25) {
+            prim=prim*2;
+        }
+        System.out.println("Have you had any accidents in the last 5 years?");
+        String accident=scan.nextLine();
+        if (accident == "yes" || accident=="YES") {
+            System.out.println("How many?");
+            int acci=scan.nextInt();
+            prim=prim+acci*10;
+        }
+        System.out.println("What is the highest level of education you have completed?");
+        String edu=scan.nextLine();
+        if (edu=="Bachelors" || edu=="Masters"){
+            prim=prim-(prim*0.05);
+        } else if (edu=="PHD") {
+            prim=prim-(prim*0.1);
+        }else if (edu == "High School") {
+            prim=prim+(prim*0.05);
+        }
+
+        String firsName,lastName,a;
+        firsName=fullName.substring(0,fullName.indexOf(" "));
+        a=firsName.substring(0,1);
+        a=a.toUpperCase();
+        firsName=a+firsName.substring(1);
+        lastName=fullName.substring(fullName.indexOf(" ")+1);
+        a=lastName.substring(0,1);
+        a=a.toUpperCase();
+        lastName=a+lastName.substring(1);
+        System.out.println(firsName+" "+lastName+" here's your quote!");
+        System.out.println("This is your start premium cost: $"+ prim);
+        String refNo=firsName.substring(0,2)+lastName.substring(lastName.length()-3)+zipCode+edu;
+        System.out.println("This is your reference number: "+refNo);
+    }
+}
--- src/RegexKullan.java
+++ src/RegexKullan.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class RegexKullan {
     public static void main(String[] args) {
         String asd="selam nasılsın REgex kullanma denemesi yapıyorum";
         System.out.println(asd.matches(".*dem.*"));
         System.out.println(asd.matches("[^a-z]"));
         System.out.println(asd.matches("[^a]"));
+
+
+
+                //DO NOT TOUCH FOLLOWING LINE/LINES
+                Scanner scan = new Scanner(System.in);
+                String word = scan.next();
+                int midle=0;
+                //WRITE YOUR CODE BELOW
+                if(word.length()%2==0){//if the word length is even
+                    midle=word.length()%2;
+                    System.out.println(word.charAt(midle-1)+word.charAt(midle));
+                }else{// if the word length is odd
+                    midle=word.length()%2;
+                    System.out.println(word.charAt(midle+1));
+                }
+
+
         //hacherrank çözümü
         /*
         Pattern Syntax Checker
