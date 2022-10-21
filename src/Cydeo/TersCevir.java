2419dd00d62fc62563c92170e75f20b92acd8caf
muratA <muratriyum@gmail.com>
2022-10-21 10:28:26 +0300

    Cydeo insurance quote solicion Bazı denemeler

--- src/Cydeo/TersCevir.java
+++ src/Cydeo/TersCevir.java
@@ -2,8 +2,25 @@ package Cydeo;
 
 import java.util.Scanner;
 
+
 public class TersCevir {
 
+
+    public static String revWords(String str) {
+        StringBuilder reversedWords = new StringBuilder();
+
+        // split input string by " " space to get each word as String[]
+        String[] words = str.split(" ");
+
+        // loop over the array from back
+        for(int i = words.length - 1; i >= 0; i--) {
+            // add words to reversedWords with space
+            reversedWords.append(words[i] + " ");
+        }
+
+        // trim needed to remove last space in the end
+        return reversedWords.toString().trim();
+    }
     public static String[] terscevir(String[] str){
         String[] asd=new String[str.length];
         int count= 0;
@@ -28,7 +45,9 @@ public class TersCevir {
         for (int i = 0; i < d.length; i++) {
             System.out.print(d[i]);
         }
+        System.out.println("");
 
+        System.out.println(revWords(word));
 
 
 
